302ca15b9610ef6949a0841a4dd2dd01c683667c
machiobrian <machiobrian29@gmail.com>
2024-01-12 11:01:45 +0300

    Fails -  docker-compose exec app php artisan link:new

--- app/Console/Commands/LinkNew.php
+++ app/Console/Commands/LinkNew.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Link;
+use App\Models\LinkList;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -39,24 +40,37 @@ class LinkNew extends Command
      */
     public function handle()
     {
-        $url = $this->ask('Link URL:');
+        $url = $this->ask('Link URL:'); //user input: command line
 
+        // the input url is validated
         if (!filter_var($url, FILTER_VALIDATE_URL)) {
             $this->error("Invalid URL. Exiting...");
             return 1;
         }
-
+        // optionally, we are asked for a description
         $description = $this->ask('Link Description:');
+        $list_name = $this->ask('Link List (Leave blank to use default)')?? "default";
+
 
         $this->info("New Link:");
         $this->info($url . ' - ' . $description);
+        $this->info("Listed in: " . $list_name);
 
+        // prompted to confirm
         if ($this->confirm('Is this information correct?')) {
+            $list = LinkList::firstWhere('slug', $list_name);
+            if (!$list){
+                $list = new LinkList();
+                $list->title = $list_name;
+                $list->slug = $list_name;
+                $list->save();
+            }
+
             $link = new Link();
             $link->url = $url;
             $link->description = $description;
-            $link->save();
-
+            $link->links()->save();
+            // if yes/y it is saved & console logged
             $this->info("Saved.");
         }
 
